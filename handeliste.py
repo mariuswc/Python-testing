12034f54f2b11315fbdd258f3dd971ce5584b676
mariuswc <mariuswc@gmail.com>
2025-05-06 12:47:32 +0200

    Update handeliste.py
    Lagt til timestamps
    sortering

--- handeliste.py
+++ handeliste.py
@@ -1,21 +1,29 @@
-
-menu = ["Add", "Remove", "Show", "Quit"]
+from datetime import datetime
+menu = ["Add", "Remove", "Show", "Quit","Clear"]
 grocerylist = []
 
+try:
+            with open("handleliste.txt", "r") as file:
+                for line in file:
+                    grocerylist.append(line.strip())
+except FileNotFoundError:
+            grocerylist = []
 
 def shoppinglist_menu(): 
         while True:
-            userinput = input(f"Enter what you want from the {menu} ").capitalize()
-            if userinput == "Quit":
-                print("The shopping list has closed")
+            userinput = input(f"Enter what you want from the list {menu}: ").upper().strip()
+            if userinput == "QUIT":
+                quitting()
                 break
-            elif userinput == "Add":
-                adding_menu()
-            elif userinput == "Remove":
-                remove_menu()
-            elif userinput == "Show":
+            elif userinput == "SHOW":
                 show_list()
                 shoppinglist_menu()
+            elif userinput == "ADD":
+                adding_menu()
+            elif userinput == "REMOVE":
+                remove_menu()
+            elif userinput == "CLEAR":
+                 clear_list()
             else:
                 print("Please enter a valid option")
     
@@ -28,9 +36,11 @@ def adding_menu():
         elif what_to_add in grocerylist:
             print("The item already exist")
         else:
-            grocerylist.append(what_to_add)
-            print(f"the list now contains: {grocerylist}")
-    
+            dt = datetime.now().strftime("%Y-%m-%d %H:%M")
+            grocerylist.append(f"{what_to_add} - {dt}")
+            grocerylist.sort()
+            print(f"The list now contains {grocerylist} at {dt}" )
+            saving_file()
 
 def remove_menu():
     if grocerylist:
@@ -41,6 +51,7 @@ def remove_menu():
             else:
                 grocerylist.remove(what_to_remove)
                 print(f"You removed {what_to_remove} from the grocerylist")
+                saving_file()
                 if not grocerylist:
                     print("The list is empty")
                     adding_menu()
@@ -56,23 +67,60 @@ def remove_menu():
 
 
 def show_list():
-  for x, item in enumerate(grocerylist, 1):
-     print(f"{x}:{item}")
+    if not grocerylist:
+        print("The list is empty")
+    else:
+        for x, item in enumerate(grocerylist, 1):
+            print(f"{x}:{item}")
 
+def clear_list():
+    
+    if not grocerylist:
+        print("The list is already empty")
+        return
+            
+    print("Current list:")
+    for item in grocerylist:
+        print(f"- {item}") 
+            
+    areyousure = input(f"Are you sure you want to clear the list? (Y or N): ").upper().strip()
+    
+    if areyousure == "Y":
+        grocerylist.clear()
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M") 
+        print(f"The list has been cleared at {timestamp}")
+        grocerylist.append(f"List cleared at {timestamp}")  
+        saving_file()
+            
+    else:
+        print("The list has not been cleared.")
+ 
 
+def quitting():
+    print("The session has quit")
+    return
 
+def saving_file():
+    with open("handleliste.txt", "w") as file:
+            for item in grocerylist:
+                file.write(item + "\n")
+        
+                
 def main():
-    shoppinglist_menu()
+        shoppinglist_menu()
+        saving_file()
+        
+
 main()
 
 
 
 
 # neste oppgave:
-# Save/load the list from a file (with open(), readlines(), etc.)
+# Save/load the list from a file (with open(), readlines(), etc.) check!
 
-#Add a feature to clear the whole list
+#Add a feature to clear the whole list check!
 
-#Add timestamps to items or mark them as "bought"
+#Add timestamps to items or mark them as "bought"check!
 
-#Sort the list alphabetically
+#Sort the list alphabetically check!
